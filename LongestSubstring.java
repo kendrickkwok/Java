c57ee7c1e7c9c8a7c3e5a27cc56171b3392071d8
Kendrick Kwok <kendrick.j.kwok@gmail.com>
2017-09-12 21:30:19 -0700

    Update LongestSubstring.java

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -4,87 +4,55 @@
  * and open the template in the editor.
  */
 package ArraysAndStrings;
+
 import java.util.*;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 /**
  *
  * @author Joseph
  */
 public class LongestSubstring {
-       public static void main(String args[])
-       {
-       System.out.println(findingSubstring("catacatsdog"));
-       }
-      
-       public static int findingFreqLength(String str)
-       {
-          int n = str.length();
-          Set<Character> set = new HashSet<>();
-          int ans = 0;
-          int left = 0;
-          int right = 0;
-          
-          //Iterate both left and right 
-          while(left < n && right < n)
-          {
-              //if set does not contain the index
-              if(!set.contains(str.charAt(right)))
-              {
-                  set.add(str.charAt(right));
-                  right++;
-                  ans = Math.max(ans, right - left);
-              }
-              
-              else
-              {
-                  //Then the set does contain the letter
-                  set.remove(str.charAt(left));
-                  left++;
-              }
-          }
-           return ans;
-       }
-       
-       public static String findingSubstring(String str)
-       {
-          Set<Character> set = new HashSet<Character>();
-        char[] arr = str.toCharArray();
-        String substring = new String();
+    public static void main(String args[])
+    {
+       System.out.println(uniqueSubString("catcatcatcatdogscs"));
+    }   
+    
+    public static String uniqueSubString(String str)
+    {
+        Set<Character> set = new HashSet<Character>();
+        int L = 0;
+        int R = 0;
         String temp = "";
-        StringBuilder sb = new StringBuilder();
-        int ans = 0;
-        int left = 0;
-        int right = 0;
-        
-        while(right != arr.length)
+        String max = "";
+       
+        while(R < str.length()-1)
         {
-            if(!set.contains(arr[right]))
+            if(!set.contains(str.charAt(R)))
             {
-                set.add(arr[right]);
-                sb.append(arr[right]);
-                right++;
-                //ans = Math.max(ans, right-left);
+                set.add(str.charAt(R));
+                R++;
             }
             else
             {
-                set.clear();
-                temp = sb.toString();
-                sb.setLength(0);
-                
-                if(temp.length() > substring.length())
+                temp = str.substring(L, R);
+                if(temp.length() > max.length())
                 {
-                    substring = temp;
+                    max = temp;
                 }
+                set.remove(str.charAt(L));
+                L++;
             }
-            
         }
-         temp = sb.toString();
-         if(temp.length() > substring.length())
-         {
-            substring = temp;
-         }
-        return substring;
-       }
-       
+        
+        temp = str.substring(L, R);
+                if(temp.length() > max.length())
+                {
+                    max = temp;
+                }
+        return max;       
+    }
+    
 }
