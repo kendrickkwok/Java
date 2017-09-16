deaeccf6b3bf45213d00643ea6ad367f0839218b
Kendrick Kwok <kendrick.j.kwok@gmail.com>
2017-09-15 22:22:28 -0700

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -11,6 +11,10 @@ package ArraysAndStrings;
  */
 public class ReverseString {
     
+    public static void main (String[] args)
+    {
+        reverseString3("Hello");
+    }
     public String reverseString(String str)
     {
         int len = str.length()-1;
@@ -23,5 +27,34 @@ public class ReverseString {
         }
         
         return sb.toString();
+    }
+    
+    public static String reverseString2(String str)
+    {
+        StringBuilder sb  = new StringBuilder(str);
+        return sb.reverse().toString();
+        
+    }
+    
+    public static void reverseString3(String str)
+    {
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = str.length()-1;
+        char temp = 'x';
+        
+        while(left < right)
+        {
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp; 
+            left++;
+            right--;
+        }
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
     }
 }
